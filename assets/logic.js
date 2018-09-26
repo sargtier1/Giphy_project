4dcc23be6b02821d3c8db74f4715cdc2f90c8f6b
JustTams <sarg.tier1@gmail.com>
2018-09-25 21:37:56 -0500

    just need clear button

--- assets/logic.js
+++ assets/logic.js
@@ -1,13 +1,17 @@
-// Variables //
-///////////////
-
 // Array for user search terms //
-var searchArr = ["dog", "cat", "bird"];
+var searchArr = ["cat", "lion", "tiger"];
 
-// API key
-// 6AnWVicwRJqEOwWsff57eY2nW2RyHnR9
+// search function
+$("#search-btn").on("click", function () {
 
-// arbitrary unction to create BS buttons
+    searchedItem = $("#search-term").val();
+    searchArr.push(searchedItem);
+    createBtn(searchArr, "btn btn-primary search-button", ".search-btns");
+    console.log(searchedItem);
+    console.log(searchArr);
+});
+
+// arbitrary function to create BS buttons/ add class, where to add each gif
 function createBtn (searchArr, classToAdd, areaToAdd) {
     $(areaToAdd).empty();
     for (i=0;i < searchArr.length;i++) {
@@ -16,19 +20,55 @@ function createBtn (searchArr, classToAdd, areaToAdd) {
         btn.attr("data-searched", searchArr[i]);
         btn.text(searchArr[i]);
         $(areaToAdd).append(btn);
+        $(btn).after(" ");
     }
 };
 
-// Pre-set Buttons on load
+// Pre-set Buttons on load function call
 $(function () {
-    createBtn(searchArr, "btn btn-primary", ".lead");
+    createBtn(searchArr, "btn btn-primary search-button", ".search-btns");
 });
 
-$(document).on("click", function () {
-    var searched = $(this).data("data-searched");
-    console.log(searched); // shows undefined in console
+$(document).on("click", ".search-button", function () {
 
+    $(".searched-gifs").empty();
+
+    var searched = $(this).attr("data-searched");
+    console.log(this);
+    
+    console.log("searched: " + searched); // shows undefined in console
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+        searched + "&apikey=6AnWVicwRJqEOwWsff57eY2nW2RyHnR9";
+
+// API key & AJX call
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+
+.then(function(response) {
+    console.log (queryURL);
+    console.log (response);
+
+var results =response.data;
+console.log(results);
+
+for (var i=0; i < 10; i++) {
+    var searchDiv = $("<div>");
+    var p = $("<p>").text("Rating: " + results[i].rating )
+
+    var searchImage = $("<img>");
+    searchImage.attr("src", results[i].images.fixed_height.url);
+
+    console.log(results[i].images.fixed_height.url);
+
+    searchDiv.prepend(p);
+    searchDiv.prepend(searchImage);
+    //inlcude copy link here
+
+    $(".searched-gifs").append(searchDiv);
+}
+})
 });
 
-
-{/* <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> */}
